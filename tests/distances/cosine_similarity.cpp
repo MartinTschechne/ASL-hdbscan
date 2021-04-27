ce0d94d7a0c18b7bedc5ede0ffa08f9dbfc2964b
Mark Frey <freymark.dev@gmail.com>
2021-04-27 09:34:26 +0200

    added unit test

---
+++ tests/distances/cosine_similarity.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include <distances/cosine_similarity.h>
+
+TEST(distances, cosine_similarity_similar) {
+    double a[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
+    double b[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+    double sim = CosineSimilarity(a, b, 5);
+
+    ASSERT_NEAR(sim, 0.0, 0.0001);
+}
+
+TEST(distances, cosine_similarity_ortho) {
+    double a[3] = {2.0, -4.2, -3.15};
+    double b[3] = {-21.84, -21.155, 14.34};
+
+    double sim = CosineSimilarity(a, b, 5);
+
+    ASSERT_NEAR(sim, 1.0, 0.0001);
+}
+
+TEST(distances, cosine_similarity1) {
+    double a[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
+    double b[5] = {2.5, 8.5, 2.0, 1.0, 3.0};
+
+    double sim = CosineSimilarity(a, b, 5);
+
+    ASSERT_NEAR(sim, 0.376110322, 0.0001);
+}
