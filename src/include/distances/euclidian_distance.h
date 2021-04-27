9cddf4d263fa9ac3438ec2aeb3769f128cf01d0a
Mark Frey <freymark.dev@gmail.com>
2021-04-27 09:56:11 +0200

    Euclidian doc string

--- src/include/distances/euclidian_distance.h
+++ src/include/distances/euclidian_distance.h
@@ -1,8 +1,16 @@
-#ifndef TEAM33_DISTANCES_EUCLIDIAN_DISTANCES
-#define TEAM33_DISTANCES_EUCLIDIAN_DISTANCES
+#ifndef TEAM33_DISTANCES_EUCLIDIAN_DISTANCE_H
+#define TEAM33_DISTANCES_EUCLIDIAN_DISTANCE_H
 
 #include <cmath>
 
+/**
+ * @brief Computes the Euclidian distance between a and b
+ * 
+ * @param a Vector of length >= n
+ * @param b Vector of length >= n
+ * @param n Size of the vectors
+ * @return Euclidian distance
+ */
 inline double EuclidianDistance(double* a, double* b, size_t n) {
     double distance = 0.0;
     double diff = 0.0;
