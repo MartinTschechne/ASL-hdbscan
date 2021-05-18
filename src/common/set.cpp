84f213999b2a630ccf75c17d14df8cf0bac714d6
Beat Hubmann <BeatHubmann@users.noreply.github.com>
2021-05-18 16:33:03 +0200

    Implement set functionality for size_t in C

--- src/common/set.cpp
+++ src/common/set.cpp
@@ -1,3 +1,279 @@
 #include <common/set.h>
 #include <cstdlib>
+#include <cstring>
 
+// bitset - required for set:
+
+bitset* bitset_create(size_t num_bits) {
+    bitset* b = (bitset*)malloc(sizeof(*b));
+    b->num_bits = num_bits;
+    b->num_words = (size_t)(num_bits / bitword_size) + 1;
+    b->words = (bitword_t*)malloc(b->num_words * sizeof(*b->words));
+    bitset_clear_all(b);
+    return b;
+}
+
+void bitset_free(bitset* b) {
+    free(b->words);
+    free(b);
+}
+
+void bitset_set(bitset* b, size_t bit) {
+    b->words[bit_idx(bit)] |= (1ULL << bit_offset(bit));
+}
+
+void bitset_clear(bitset* b, size_t bit) {
+    b->words[bit_idx(bit)] &= ~(1ULL << bit_offset(bit));
+}
+
+size_t bitset_get(bitset* b, size_t bit) {
+    return b->words[bit_idx(bit)] & (1ULL << bit_offset(bit));
+}
+
+void bitset_clear_all(bitset* b) {
+    memset(b->words, 0, b->num_words * sizeof(*b->words));
+}
+
+void bitset_resize(bitset* b, size_t capacity) {
+    b->words = (bitword_t*)realloc(
+            b->words, capacity * sizeof(*b->words));
+    for (size_t i = b->num_bits; i < capacity; i++) {
+        bitset_clear(b, i);
+    }
+    b->num_bits = capacity;
+    b->num_words = (size_t)(capacity / bitword_size) + 1; 
+}
+
+static inline size_t bit_idx(size_t bit) {
+    return (size_t)(bit / bitword_size);
+}
+
+static inline size_t bit_offset(size_t bit) {
+    return (size_t)(bit % bitword_size);
+}
+
+// set:
+
+set* set_create() {
+    set* s = (set*)malloc(sizeof(*s));
+    s->capacity = SET_DEFAULT_CAPACITY;
+    s->size = 0;
+    s->elements = (size_t*)malloc(s->capacity * sizeof(size_t));
+    s->bit_elements = bitset_create(s->capacity);
+    return s;
+}
+
+void set_free(set* s) {
+    free(s->elements);
+    free(s->bit_elements);
+    free(s);
+}
+
+size_t set_begin(const set* s) {
+    size_t idx = UNDEFINED_VALUE;
+    if (s) {
+        if (s->size > 0) {
+            idx = 0;
+            while (idx < s->capacity && !bitset_get(s->bit_elements, idx)) {
+                idx++;
+            }
+        }
+        else {
+            idx = set_end(s);
+        }
+    }
+    return idx;
+}
+
+size_t set_next(const set* s, size_t idx) {
+    size_t next = UNDEFINED_VALUE;
+    if (s && s->capacity > 0) {
+        next = idx + 1;
+        while (next < s->capacity) {
+            if (bitset_get(s->bit_elements, next)) {
+                return next;
+            }
+            next++;
+        }
+        next = idx + 1;
+    }
+    return next;
+}
+
+// size_t set_prev(const set* s, size_t idx) {
+//     size_t prev = UNDEFINED_VALUE;
+//     if (s && s->capacity > 0) {
+//         prev = idx - 1;
+//         while (prev >= 0) {
+//             if (bitset_get(s->bit_elements, prev)) {
+//                 return prev;
+//             }
+//             prev--;
+//         }
+//         prev = set_end(s);
+//     }
+//     return prev;
+// }
+
+size_t set_end(const set* s) {
+    size_t idx = UNDEFINED_VALUE;
+    if (s) {
+        if (s->size > 0) {
+            idx = s->capacity;
+            while (0 < idx && !bitset_get(s->bit_elements, idx-1)) {
+                idx--;
+            }
+        }
+        else {
+            idx = 0;
+        }
+    }
+    return idx;
+}
+
+size_t set_get(const set* s, size_t idx) {
+    size_t key = UNDEFINED_VALUE;
+    if (s && s->size > 0) {
+        if (bitset_get(s->bit_elements, idx)) {
+            key = s->elements[idx];
+        }
+    }
+    return key;
+}
+
+bool set_empty(const set* s) {
+    return (s->size == 0);
+}
+
+size_t set_size(const set* s) {
+    return s->size;
+}
+
+void set_clear(set* s) {
+    if (s && s->size > 0)  {
+        s->size = 0;
+        bitset_clear_all(s->bit_elements);
+    }
+}
+
+void set_insert(set* s, size_t key) {
+    if (s) {
+        if (!set_contains(s, key)) {
+            if ((float)(s->size / s->capacity) > SET_MAX_USAGE_RATIO) {
+                set_resize(s, 2 * s-> capacity);
+                }
+            size_t idx = key % s->capacity;
+            idx = set_next_open_idx(s, idx);
+            s->elements[idx] = key;
+            bitset_set(s->bit_elements, idx);
+            s->size++;
+        }
+    }
+}
+
+void set_erase(set* s, size_t key) {
+    if (s && s->size > 0) {
+        size_t idx = set_find(s, key);
+        if (idx != set_end(s)) {
+            set_clear_idx(s, idx);
+            s->size--;
+        }
+    }
+}
+
+size_t set_find(const set* s, size_t key) {
+    size_t idx = UNDEFINED_VALUE;
+    if (s && s->size > 0) {
+        idx = key % s->capacity; 
+        if (!bitset_get(s->bit_elements, idx)) {
+            idx = set_next_busy_idx(s, idx);
+        }
+        size_t start_idx = idx;
+        do {
+            idx = set_next_busy_idx(s, idx);
+            if (s->elements[idx] == key) {
+                return idx;
+            }
+        }  while (idx != start_idx);
+        idx = set_end(s);
+    }
+    return idx;
+}
+
+bool set_contains(const set* s, size_t key) {
+    bool contains = false;
+    if (s && s->size > 0) {
+        contains = (set_find(s, key) != set_end(s));
+    }
+    return contains;
+}
+
+void set_resize(set* s, size_t capacity) {
+    if (s && (capacity > s->capacity)) {
+        s->elements = (size_t*)realloc(
+            s->elements, capacity * sizeof(size_t));
+        bitset_resize(s->bit_elements, capacity);
+        s->capacity = capacity;
+        set_reorder(s, 0);
+    }
+}
+
+static void set_reorder(set* s, size_t idx) {
+    if (s) {
+        size_t next = idx;
+        while (next < s->capacity) {
+            if (bitset_get(s->bit_elements, next)) {
+                size_t key = s->elements[next];
+                size_t target_idx = key % s->capacity;
+                if (next != target_idx) {
+                    target_idx = set_next_open_idx(s, target_idx);
+                    if (target_idx < s->capacity) {
+                        s->elements[target_idx] = s->elements[next];
+                        bitset_set(s->bit_elements, target_idx);
+                        set_clear_idx(s, next);
+                        bitset_clear(s->bit_elements, next);
+                    }
+                }
+            }
+            next++;
+        }
+    }
+}
+
+static void set_clear_idx(set* s, size_t idx) {
+    if (s) {
+        s->elements[idx] = UNDEFINED_VALUE;
+        bitset_clear(s->bit_elements, idx);
+    }
+}
+
+static size_t set_next_busy_idx(const set* s, size_t idx) {
+    size_t next = UNDEFINED_VALUE;
+    if (s && s->capacity > 0) {
+        next = idx + 1;
+        while (true) {
+            if (next == s->capacity) {
+                next = 0;
+            }
+            if (next == idx) {
+                break;
+            }
+            if (bitset_get(s->bit_elements, next)) {
+                break;
+            }
+            next++;
+        }
+    }
+    return next;
+}
+
+static size_t set_next_open_idx(const set* s, size_t idx) {
+    size_t next = idx;
+    while (bitset_get(s->bit_elements, next) && next < s->capacity) {
+        next++;
+        if (next == s->capacity) {
+            next = 0;
+        }
+    }
+    return next;
+}
--- src/include/common/set.h
+++ src/include/common/set.h
@@ -1,28 +1,111 @@
 #ifndef TEAM33_SET_H
 #define TEAM33_SET_H
 
-#define SET_DEFAULT_CAPACITY 10
-#define SET_MAX_USAGE 0.25
+#define SET_DEFAULT_CAPACITY 512
+#define SET_MAX_USAGE_RATIO 0.25
 #define UNDEFINED_VALUE -1
 
 #include <cstddef>
+#include <cinttypes>
 
-typedef struct SetNode {
-    size_t element;
-} set_node;
+// bitset - required for set:
+
+typedef uint64_t bitword_t;
+enum {
+    bitword_size = 8ULL * sizeof(bitword_t)
+};
+typedef struct BitSet {
+    bitword_t* words;
+    size_t num_bits;
+    size_t num_words;
+} bitset;
+
+/**
+ * @brief Allocates memory and initializes an empty zeroed bitset with set number of bits
+ * 
+ * @param num_bits Number of bits in the bitset
+ * @return bitset* Pointer to the initialized bitset
+ */
+bitset* bitset_create(size_t num_bits);
+
+/**
+ * @brief Releases memory allocated to a bitset
+ * 
+ * @param b Bitset to release memory from 
+ */
+void bitset_free(bitset* b);
+
+/**
+ * @brief Sets a bit in a bitset
+ * 
+ * @param b Bitset to set bit in
+ * @param bit Bit to set
+ */
+void bitset_set(bitset* b, size_t bit);
+
+/**
+ * @brief Clears a it in a bitset
+ * 
+ * @param b Bitset to clear bit in
+ * @param bit Bit to clear
+ */
+void bitset_clear(bitset* b, size_t bit);
+
+/**
+ * @brief Retrieves a bit from a bitset
+ * 
+ * @param b Bitset to retrieve bit from 
+ * @param bit Bit to retrieve
+ * @return size_t Content of bit
+ */
+size_t bitset_get(bitset* b, size_t bit);
+
+/**
+ * @brief Clears (i.e.) zeroes a bitset
+ * 
+ * @param b Bitset to clear
+ */
+void bitset_clear_all(bitset* b);
+
+/**
+ * @brief Resizes a bitset to a given capacity
+ * 
+ * @param b Bitset to resize
+ * @param capacity New capacity of bitset
+ */
+void bitset_resize(bitset* b, size_t capacity);
+
+/**
+ * @brief Returns the bitword index of a given bit
+ * 
+ * @param bit Bit to get the bitword index of
+ * @return size_t Index of bitword which bit belongs to
+ */
+static inline size_t bit_idx(size_t bit);
+
+/**
+ * @brief Returns the offset of a bit within a bitword
+ * 
+ * @param bit Bit to get the offset within a word of
+ * @return size_t Offset of bit within the bitword bit belongs to
+ */
+static inline size_t bit_offset(size_t bit);
+
+// set:
 
 typedef struct Set {
     size_t capacity;
     size_t size;
-    void** elements;
+    size_t* elements;
+    bitset* bit_elements;
 } set;
 
 /**
- * @brief Allocates memory and initializes a set with default capacity
+ * @brief Allocates memory and initializes an empty set with default capacity
  * 
- * @param s Set to initialize
+ * @return set* 
  */
-void set_init(set* s);
+set* set_create();
 
 /**
  * @brief Releases memory previously allocated to set
@@ -32,33 +115,43 @@ void set_init(set* s);
 void set_free(set* s);
 
 /**
- * @brief Imitates iterator by returning first element of set
+ * @brief Imitates iterator by returning index of first member of set
  * 
- * @param s Set to get first element of
- * @return size_t Value of first element of set s
+ * @param s Set to get the index of the first member of
+ * @return size_t Index of first member of set s
  */
 size_t set_begin(const set* s);
 
 /**
- * @brief Imitates iterator by returning following element of set
+ * @brief Imitates iterator by returning next index for given index
+ * of member of set
  * 
- * @param s Set to get next element of
- * @param value Value to get next element of
- * @return size_t Value of element which follows value in set s
+ * @param s Set to get next index of given index from
+ * @param idx Index to get followup index of
+ * @return size_t Index of member which follows index idx in set s
  */
-size_t set_next(const set* s, size_t value);
+size_t set_next(const set* s, size_t idx);
 
 /**
- * @brief Imitates iterator by returning last element of set
+ * @brief Imitates iterator by returning index+1 of last member of set
  * 
- * @param s Set to get last element of
- * @return size_t Value of last element of set s
+ * @param s Set to get last index of member of
+ * @return size_t Index+1 of last member of set s
  */
 size_t set_end(const set* s);
 
+/**
+ * @brief Imitates iterator by getting the key at a given index in a set
+ * 
+ * @param s Set to get key from 
+ * @param idx Index from which to get the key from
+ * @return size_t Key at index idx in set s
+ */
+size_t set_get(const set* s, size_t idx);
+
 
 /**
- * @brief Checks if the set has no elements
+ * @brief Checks if the set has no members
  * 
  * @param s Set to check emptyness of
  * @return true If the set is empty
@@ -67,18 +160,18 @@ size_t set_end(const set* s);
 bool set_empty(const set* s);
 
 /**
- * @brief Returns the number of elements in the set
+ * @brief Returns the number of members in the set
  * 
- * @param s Set to get number of elements of
- * @return size_t The numer of elements in the set
+ * @param s Set to get number of members of
+ * @return size_t The numer of members in set s
  */
 size_t set_size(const set* s);
 
 /**
- * @brief Erases all elements from the set. After this call, set_size()
+ * @brief Erases all members from the set. After this call, set_size()
  * returns zero
  * 
- * @param s Set to erase all elements from
+ * @param s Set to erase all members from
  */
 void set_clear(set* s);
 
@@ -91,20 +184,72 @@ void set_clear(set* s);
 void set_insert(set* s, size_t value);
 
 /**
- * @brief Erase a value from a set if present
+ * @brief Erases a key from a set if present
  * 
- * @param s Set to erase value from
- * @param value Value to erase from set
+ * @param s Set to erase key from
+ * @param key Key to erase from set
  */
-void set_erase(set* s, size_t value);
+void set_erase(set* s, size_t key);
 
 /**
- * @brief Find a value in a set
+ * @brief Finds a key in a set
  * 
- * @param s Set to find value in 
- * @param value Value to find in set
- * @return size_t TBD
+ * @param s Set to find key in 
+ * @param key Key to find in set
+ * @return size_t Iterator-imitating index of element in set if found
  */
-size_t set_find(const set* s, size_t value);
+size_t set_find(const set* s, size_t key);
+
+/**
+ * @brief  Checks if a key is member of a set
+ * 
+ * @param s Set to check if key is member of
+ * @param key Key to check set for
+ * @return true If the key is contained in set s
+ * @return false Otherwise
+ */
+bool set_contains(const set* s, size_t value); 
+
+/**
+ * @brief Resize the set to capacity
+ * 
+ * @param s Set to resize
+ * @param capacity Target capacity of resized set 
+ */
+void set_resize(set* s, size_t capacity);
+
+/**
+ * @brief Reorder the set starting from given index to keep ordering
+ * 
+ * @param s Set to reorder 
+ * @param idx Index to start reordering from 
+ */
+static void set_reorder(set* s, size_t idx);
+
+/**
+ * @brief Removes the key at an index from a set
+ * 
+ * @param s Set to remove key from 
+ * @param idx Index at which to remove key
+ */
+static void set_clear_idx(set* s, size_t idx);
+
+/**
+ * @brief Finds the next occupied index in a set
+ * 
+ * @param s Set to find next occupied index in
+ * @param idx Start index from which the next occupied index is wanted
+ * @return size_t Next occupied index in set s
+ */
+static size_t set_next_busy_idx(const set* s, size_t idx);
+
+/**
+ * @brief Finds the next unoccupied index in a set
+ * 
+ * @param s Set to find next unoccupied index in
+ * @param idx Start index from which the next unoccupied index is wanted
+ * @return size_t Next unoccupied index in set s
+ */
+static size_t set_next_open_idx(const set* s, size_t idx);
 
 #endif
