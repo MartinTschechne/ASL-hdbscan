08e62cec266929871ef291d582193f7ecd63b8c0
Robin Worreby <rworreby@student.ethz.ch>
2021-05-25 15:06:49 +0200

    Add plotting script for measurements

--- plotting/plot_measurements.py
+++ plotting/plot_measurements.py
@@ -10,6 +10,6 @@ the compiler optimization level and the dimension of the data set.\n \
     Usage: python plot_measurements.txt measurements.csv 3 32"
 
 df = pd.read_csv(sys.argv[1], skiprows=4, index_col='Region')
-df.plot(kind='bar')
+df.plot(kind='bar', logy=True)
 plt.title(f'Cycle measurements for -O{sys.argv[2]}, dim(data)={sys.argv[3]}')
 plt.savefig('measurement.png', bbox_inches='tight')
